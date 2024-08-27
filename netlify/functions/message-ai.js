4a4812ea109c7eb58c83d5206dbbd9097558ce8f
Stav Geffen <stavgeffen@gmail.com>
2024-08-27 12:28:37 +0400

    fix(openai) sets apikey explicitely

--- netlify/functions/message-ai.js
+++ netlify/functions/message-ai.js
@@ -1,6 +1,7 @@
 const OpenAI = require("openai");
 
 const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
 	organization: process.env.OPENAI_ORG,
 	project: process.env.OPENAI_PROJECT,
 });
