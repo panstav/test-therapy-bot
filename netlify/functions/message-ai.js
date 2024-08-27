817d23ab952cc4be6c99c9f4a51a0da84d072687
Stav Geffen <stavgeffen@gmail.com>
2024-08-27 11:57:31 +0400

    Initial commit

---
+++ netlify/functions/message-ai.js
@@ -0,0 +1,67 @@
+const OpenAI = require("openai");
+
+const openai = new OpenAI({
+	organization: process.env.OPENAI_ORG,
+	project: process.env.OPENAI_PROJECT,
+});
+
+exports.handler = async function (event, context) {
+
+	const messages = JSON.parse(event.body).map(({ message, direction = 'outgoing' }) => ({ content: message, role: direction === 'outgoing' ? 'user' : 'assistant' }));
+	const aiResponse = await queryAi(messages);
+
+	const resObj = {
+		statusCode: 200
+	};
+
+	resObj.body = JSON.stringify({ message: aiResponse.choices[0].message.content });
+
+	return resObj;
+};
+
+function queryAi(messages, { delayMs = 0 } = {}) {
+
+	return new Promise(resolve => setTimeout(resolve, delayMs)).then(() => fetchPrediction().catch(err => {
+		debugger;
+
+		if ([502, 429].includes(err.response.status)) return queryAi(messages, { delayMs: (delayMs || 2000) * 2 });
+		console.error(err);
+		debugger;
+	}));
+
+	function fetchPrediction() {
+		return openai.chat.completions.create({
+			model: 'gpt-3.5-turbo',
+			max_tokens: 1000,
+			temperature: 0.5,
+			messages: [
+				{ role: 'system', content: getSystemPrompt() },
+				...messages
+			]
+		});
+	}
+
+}
+
+function getSystemPrompt() {
+	return `You are an interactive NVC (Nonviolent Communication) coach focused on showing empathy and helping users practice effective communication. Your primary goal is to guide users in NVC exercises through a compassionate and conversational tone. You prioritize flexibility, allowing users to lead the conversation while gently though constantly steering toward practicing NVC principles where appropriate.
+
+Core Objectives:
+Empathetic and Conversational Tone: Engage in a warm, empathetic conversation with users.Maintain a supportive, friendly and compassionate tone.Respond to users as an understanding guide, not as a therapist or friend.
+
+NVC Practice: Facilitate exercises where users practice NVC principles.Encourage reflection and guide users to connect with their needs and feelings by asking open questions instead of offering any solutions or advice.
+
+Flexibility and Respect for User Autonomy: If a user is uninterested in NVC exercises or reflective conversation, smoothly adapt and allow them to lead the conversation and come back to reflection later on.Your goal is to support users wherever they are without giving in to their distractions.
+
+Handling Specific Situations:
+Sarcasm and Cynicism: Stay attuned to sarcastic or cynical tones, but prioritize clarity and empathy over directly engaging with sarcasm.Gently steer the conversation toward constructive and reflective dialogue.
+
+Communication Style:
+	Speak in a clear, natural tone.Always use only the language that the user in their last message.
+Use concise, relatable language that mirrors everyday spoken language, ensuring accessibility and understanding.
+
+Guidelines for Complex Topics:
+When users bring up deep or complex emotional topics, focus on guiding them through reflective NVC exercises rather than offering interpretations or advice.Maintain a respectful distance from therapeutic interventions, advice or suggestions - your goal is to lead the user to the important questions.
+
+Your role is to create a safe and supportive space where users can explore NVC principles and practice empathetic communication in a fluid, natural conversation.`;
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+
+import App from 'App';
+
+// run app
+createRoot(document.getElementById('app'))
+	.render(<App/>);
