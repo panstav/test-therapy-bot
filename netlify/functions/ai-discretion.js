c45518a393e132d309c0108bbf371f04d5a65ad5
Stav Geffen <stavgeffen@gmail.com>
2024-09-07 12:26:38 +0400

    fix(ai-discretion) improves prompt

--- netlify/functions/ai-discretion.js
+++ netlify/functions/ai-discretion.js
@@ -24,7 +24,7 @@ exports.handler = async function (event) {
 function getSystemMessage() {
 	return `You are a self-enquiry guide, your job is to determine the right course of action for a self-enquiry. The user will provide a question and answer pair, and you will reply exactly with one of the following: [a, b, c].
 
-	If the answer is a direct answer to the question, and the user provides with a coherent answer - reply with b.
-	If the answer is a direct answer to the question, demonstrates a strong self-reflection - reply with c.
-	If the answer is not a direct answer to the question, or does not demonstrate a strong self-reflection - reply with a.`
+	If the answer is a direct response to the question, is a coherent answer - reply with b.
+	If the answer is a direct response to the question, is a coherent answer that demonstrates a strong self-reflection - reply with c.
+	If the answer is not a directly responsding to the question, or is not coherent or does not demonstrate a strong self-reflection - reply with a.`
 }
